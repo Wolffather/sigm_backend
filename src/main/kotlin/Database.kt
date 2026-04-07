779d434818933c394b0bc805c3649c13365fb157
Wolffather <maxcross93@gmail.com>
2026-04-07 22:40:11 +0300

    Add prod settings

--- src/main/kotlin/Database.kt
+++ src/main/kotlin/Database.kt
@@ -9,11 +9,15 @@ import ru.hey_savvy.tables.RoomsTable
 import ru.hey_savvy.tables.UsersTable
 
 fun configureDatabase() {
+    val dbUrl = System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5434/sigm_db"
+    val dbUser = System.getenv("DB_USER") ?: "sigm_adm_usr"
+    val dbPassword = System.getenv("DB_PASSWORD") ?: "sigm_adm_psswrd"
+
     Database.connect(
-        url = "jdbc:postgresql://localhost:5434/sigm_db",
+        url = dbUrl,
         driver = "org.postgresql.Driver",
-        user = "sigm_adm_usr",
-        password = "sigm_adm_psswrd"
+        user = dbUser,
+        password = dbPassword
     )
 
     transaction {
